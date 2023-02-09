945a62995293aa348558aeb1aad82639c831dc2d
dhdhg <dhdhg@DAHEE>
2023-02-09 10:22:19 +0900

    2번쨰

--- teampro/src/teampro/test.java
+++ teampro/src/teampro/test.java
@@ -13,6 +13,7 @@ public class test extends Application{
 public static void main(String[] args) {
 	System.out.println("hi");
 	System.out.println("welcome");
+	System.out.println("second");
 	launch(args);
 }
 }
