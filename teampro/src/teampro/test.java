60687eaaef6e3bd6cb1df7231aea745251d0276e
dhdhg <dhdhg@DAHEE>
2023-02-09 10:24:57 +0900

    444444

--- teampro/src/teampro/test.java
+++ teampro/src/teampro/test.java
@@ -14,6 +14,7 @@ public static void main(String[] args) {
 	System.out.println("hi");
 	System.out.println("welcome");
 	System.out.println("second");
+	System.out.println("four");
 	launch(args);
 }
 }
