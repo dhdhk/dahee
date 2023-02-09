cc164d067a6b9e11242d5ce597a9a6b66beacb43
dhdhg <dhdhg@DAHEE>
2023-02-09 09:49:37 +0900

    테스트파일입니다.

---
+++ teampro/src/teampro/test.java
@@ -0,0 +1,17 @@
+package teampro;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+public static void main(String[] args) {
+	System.out.println("hi");
+	launch(args);
+}
+}
